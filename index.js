c5089b5f74c578bb1138f16c6a5ce7eeb7680252
Kluddizz <flhansen.93@gmail.com>
2020-05-26 17:17:33 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}...`);
+});
