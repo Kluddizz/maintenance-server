615c75fc42c6079bee28e8523e34ea42551e48c0
Kluddizz <flhansen.93@gmail.com>
2020-07-22 00:15:33 +0200

    Separate login routes

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const exprjwt = require("express-jwt");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const db = require("./db");
+const login = require("./routes/login");
 
 const port = process.env.PORT || 5050;
-const privateKey = fs.readFileSync(`${__dirname}/private.key`);
 const publicKey = fs.readFileSync(`${__dirname}/public.key`);
 
 const app = express();
@@ -19,53 +18,19 @@ app.use(helmet());
 app.use(cors());
 
 app.use((err, req, res, next) => {
-	if (err.name === "UnauthorizedError") {
-		res.status(403).json({
-			success: false,
-			message: "Unauthorized"
-		});
-	}
+  if (err.name === "UnauthorizedError") {
+    res.status(403).json({
+      success: false,
+      message: "Unauthorized"
+    });
+  }
 });
 
-app.post("/login", async (req, res) => {
-	const { username, password } = req.body;
-
-	const query = await db.query(
-		`
-    SELECT *
-    FROM users
-    WHERE username = $1
-          AND password = crypt($2, password);
-  `,
-		[username, password]
-	);
-
-	if (query.rows.length === 1) {
-		const user = query.rows[0];
-
-		const payload = {
-			id: user.id,
-			username: user.username
-		};
-
-		const token = jwt.sign(payload, privateKey, { algorithm: "RS256" });
-
-		res.status(200).json({
-			success: true,
-			message: "Authentication successful",
-			token: token
-		});
-	} else {
-		res.status(403).json({
-			success: false,
-			message: "Authentication failed"
-		});
-	}
-});
+app.use("/login", login);
 
 app.get("/users", async (req, res) => {
-	const query = await db.query(
-		`
+  const query = await db.query(
+    `
 		SELECT username,
 					 firstname,
 					 lastname,
@@ -73,23 +38,23 @@ app.get("/users", async (req, res) => {
 		FROM users
 		WHERE id = $1;
 	`,
-		[req.user.id]
-	);
+    [req.user.id]
+  );
 
-	if (query.rows.length === 1) {
-		res.status(200).json({
-			success: true,
-			message: "Fetched user",
-			user: query.rows[0]
-		});
-	} else {
-		res.status(400).json({
-			success: false,
-			message: "Authentication error"
-		});
-	}
+  if (query.rows.length === 1) {
+    res.status(200).json({
+      success: true,
+      message: "Fetched user",
+      user: query.rows[0]
+    });
+  } else {
+    res.status(400).json({
+      success: false,
+      message: "Authentication error"
+    });
+  }
 });
 
 app.listen(port, () => {
-	console.log(`Server running on port ${port}...`);
+  console.log(`Server running on port ${port}...`);
 });
---
+++ routes/login.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const path = require("path");
+
+const appDir = path.dirname(require.main.filename);
+const privateKey = fs.readFileSync(`${appDir}/private.key`);
+
+router.route("/").post(async (req, res) => {
+  const { username, password } = req.body;
+
+  const query = await db.query(
+    `
+      SELECT *
+      FROM users
+      WHERE username = $1
+            AND password = crypt($2, password);
+    `,
+    [username, password]
+  );
+
+  if (query.rows.length === 1) {
+    const user = query.rows[0];
+
+    const payload = {
+      id: user.id,
+      username: user.username
+    };
+
+    const token = jwt.sign(payload, privateKey, { algorithm: "RS256" });
+
+    res.status(200).json({
+      success: true,
+      message: "Authentication successful",
+      token: token
+    });
+  } else {
+    res.status(403).json({
+      success: false,
+      message: "Authentication failed"
+    });
+  }
+});
+
+module.exports = router;
