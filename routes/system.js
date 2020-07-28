0e1f672e0a240aa2f603dec25c29238812ed2cad
Kluddizz <flhansen.93@gmail.com>
2020-07-28 15:58:58 +0200

    Implement routes to get, add, delete and alter systems

--- routes/system.js
+++ routes/system.js
@@ -82,10 +82,80 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  const { name, street, city, zip, customerid } = req.body;
 
-router.put("/:id", async (req, res) => {});
+  try {
+    const query = await db.query(
+      `
+      INSERT
+      INTO systems (name, street, city, zip, customerid)
+      VALUES($1, $2, $3, $4, $5);
+    `,
+      [name, street, city, zip, customerid]
+    );
 
-router.delete("/:id", async (req, res) => {});
+    res.status(200).json({
+      success: true,
+      message: "Created new system"
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "Cannot create new system"
+    });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, street, city, zip, customerid } = req.body;
+
+  try {
+    const query = await db.query(
+      `
+      UPDATE systems
+      SET name = $1, street = $2, city = $3, zip = $4, customerid = $5
+      WHERE id = $6;
+    `,
+      [name, street, city, zip, customerid, id]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Updated system"
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "Could not update system"
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const query = await db.query(
+      `
+      DELETE
+      FROM systems
+      WHERE id = $1;
+    `,
+      [id]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Deleted system"
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "Could not delete system"
+    });
+  }
+});
 
 module.exports = router;
