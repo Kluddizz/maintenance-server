da25a49de50bedc86f56f7ae3f4f712a8253aacc
Kluddizz <flhansen.93@gmail.com>
2020-08-12 12:47:13 +0200

    Implement routes to add, update and delete maintenances.

--- routes/maintenance.js
+++ routes/maintenance.js
@@ -70,10 +70,79 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", access({ roles: ["admin"] }), async (req, res) => {});
+router.post("/", access({ roles: ["admin"] }), async (req, res) => {
+  const { name, frequency, systemid, userid, stateid } = req.body;
 
-router.put("/:id", access({ roles: ["admin"] }), async (req, res) => {});
+  try {
+    const query = await db.query(
+      `
+      INSERT INTO maintenances (name, frequency, systemid, userid, stateid)
+      VALUES ($1, $2, $3, $4, $5);
+    `,
+      [name, frequency, systemid, userid, stateid]
+    );
 
-router.delete("/:id", access({ roles: ["admin"] }), async (req, res) => {});
+    res.status(200).json({
+      success: true,
+      message: "Created new maintenance"
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "Could not create new maintenance"
+    });
+  }
+});
+
+router.put("/:id", access({ roles: ["admin"] }), async (req, res) => {
+  const { id } = req.params;
+  const { name, frequency, systemid, userid, stateid } = req.body;
+
+  try {
+    const query = await db.query(
+      `
+        UPDATE maintenances
+        SET name = $1, frequency = $2, systemid = $3, userid = $4, stateid = $5
+        WHERE id = $6;
+      `,
+      [name, frequency, systemid, userid, stateid, id]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Updated maintenance"
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "Could not update maintenance"
+    });
+  }
+});
+
+router.delete("/:id", access({ roles: ["admin"] }), async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const query = await db.query(
+      `
+        DELETE
+        FROM maintenances
+        WHERE id = $1;
+      `,
+      [id]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Deleted maintenance"
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "Could not delete maintenance"
+    });
+  }
+});
 
 module.exports = router;
