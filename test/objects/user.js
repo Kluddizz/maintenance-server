92df1d7b145723b5a97b4af24464e6c735c8ffd5
Kluddizz <flhansen.93@gmail.com>
2020-08-12 13:10:52 +0200

    Create test cases for login and user services.

---
+++ test/objects/user.js
@@ -0,0 +1,9 @@
+const user = {
+  username: "testuser1",
+  password: "test123",
+  firstName: "test",
+  lastName: "user",
+  roleid: 0
+};
+
+module.exports = user;
--- test/server.test.js
+++ test/server.test.js
@@ -3,15 +3,9 @@ const fetch = require("node-fetch");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
 
-const publicKey = fs.readFileSync(`${__dirname}/../public.key`);
+const user = require("./objects/user");
 
-const user = {
-  username: "testuser1",
-  password: "test123",
-  firstName: "test",
-  lastName: "user",
-  roleid: 0
-};
+const publicKey = fs.readFileSync(`${__dirname}/../public.key`);
 
 describe("Services", () => {
   beforeAll(async () => {
@@ -131,126 +125,6 @@ describe("Services", () => {
       // Assertions
       expect(tokenVerified).toBe(true);
       expect(forgedTokenVerified).toBe(false);
-    });
-  });
-
-  describe("User", () => {
-    let token = null;
-
-    beforeAll(async () => {
-      const request = await fetch("http://localhost:5050/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          username: user.username,
-          password: user.password
-        })
-      });
-
-      const response = await request.json();
-      token = response.token;
-    });
-
-    it("Valid get request", async () => {
-      expect.assertions(4);
-
-      const request = await fetch("http://localhost:5050/users", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-
-      const response = await request.json();
-      expect(response.success).toBe(true);
-      expect(response.user.username).toBe(user.username);
-      expect(response.user.firstname).toBe(user.firstName);
-      expect(response.user.lastname).toBe(user.lastName);
-    });
-
-    it("Update user", async () => {
-      expect.assertions(4);
-
-      const newUsername = "newusername";
-      const newPassword = "newpassword";
-
-      const request1 = await fetch("http://localhost:5050/users", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify({
-          username: newUsername
-        })
-      });
-
-      const response1 = await request1.json();
-
-      const request2 = await fetch("http://localhost:5050/users", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify({
-          username: user.username,
-          password: user.password
-        })
-      });
-
-      const response2 = await request2.json();
-
-      expect(response1.success).toBe(true);
-      expect(response1.message).toBe("Updated user");
-      expect(response2.success).toBe(true);
-      expect(response2.message).toBe("Updated user");
-    });
-
-    it("Update user with empty request", async () => {
-      expect.assertions(2);
-
-      const newUsername = "newusername";
-
-      const request = await fetch("http://localhost:5050/users", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`
-        }
-      });
-
-      const response = await request.json();
-
-      expect(response.success).toBe(true);
-      expect(response.message).toBe("Updated user");
-    });
-
-    it("False authorization", async () => {
-      expect.assertions(1);
-
-      const request = await fetch("http://localhost:5050/users", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}a`
-        }
-      });
-
-      const response = await request.json();
-      expect(response.success).toBe(false);
-    });
-
-    it("Fail without auth token", async () => {
-      expect.assertions(1);
-
-      const request = await fetch("http://localhost:5050/users", {
-        method: "GET"
-      });
-
-      const response = await request.json();
-      expect(response.success).toBe(false);
     });
   });
 });
