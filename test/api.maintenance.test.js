d71b1d9db2bbf205c9f50d9aee7092651154bde0
Kluddizz <flhansen.93@gmail.com>
2020-09-17 14:40:11 +0200

    Add empty test cases.

--- test/api.maintenance.test.js
+++ test/api.maintenance.test.js
@@ -1,2 +1,32 @@
 const db = require("../db");
 const fetch = require("node-fetch");
+
+describe("Maintenance service", () => {
+  let adminToken = null;
+  let userToken = null;
+
+  test("Get a maintenance entry", async () => {
+    
+  });
+
+  test("Get all maintenances of an user", async () => {
+
+  });
+
+  test("Get all maintenances as admin", async () => {
+
+  });
+
+  test("Create a new maintenance as admin", async () => {
+
+  });
+
+  test("Edit a mainenance as admin", async () => {
+
+  });
+
+  test("Delete maintenance as admin", async () => {
+
+  });
+
+};
