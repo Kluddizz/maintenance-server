71b7ef54548d53e06612c6d3b102a926141ba1bc
Kluddizz <flhansen.93@gmail.com>
2020-10-08 13:38:11 +0200

    Change user route.

--- routes/users.js
+++ routes/users.js
@@ -4,42 +4,36 @@ const router = express.Router();
 
 const db = require("../db");
 
-router.get("/", async (req, res) => {
+router.get("/", access({ roles: ["admin"] }), async (req, res) => {
   const query = await db.query(
     `
-		SELECT username,
+    SELECT id,
+           username,
 					 firstname,
 					 lastname,
 					 roleid
-		FROM users
-		WHERE id = $1;
+		FROM users;
 	`,
-    [req.user.id]
+    []
   );
 
-  if (query.rows.length === 1) {
-    res.status(200).json({
-      success: true,
-      message: "Fetched user",
-      user: query.rows[0]
-    });
-  } else {
-    res.status(400).json({
-      success: false,
-      message: "Authentication error"
-    });
-  }
+  res.status(200).json({
+    success: true,
+    message: "Fetched users",
+    users: query.rows,
+  });
 });
 
 router.get(
   "/:userId",
-  access({ roles: ["admin"], dataOwner: req => req.params.userId }),
+  access({ roles: ["admin"], dataOwner: (req) => req.params.userId }),
   async (req, res) => {
     const { userId } = req.params;
 
     const query = await db.query(
       `
-		SELECT username,
+		SELECT id,
+           username,
 					 firstname,
 					 lastname,
 					 roleid
@@ -53,12 +47,12 @@ router.get(
       res.status(200).json({
         success: true,
         message: "Fetched user",
-        user: query.rows[0]
+        user: query.rows[0],
       });
     } else {
       res.status(400).json({
         success: false,
-        message: "Authentication error"
+        message: "Authentication error",
       });
     }
   }
@@ -92,13 +86,13 @@ router.put("/", async (req, res) => {
 
     res.status(200).json({
       success: true,
-      message: "Updated user"
+      message: "Updated user",
     });
   } catch (err) {
     console.log(err);
     res.status(400).json({
       success: false,
-      message: "Could not update user"
+      message: "Could not update user",
     });
   }
 });
